4a0930037d0ba02036f819628ef724b1aeff3285
Ruchita Kasar <ruchikasar27@gmail.com>
2022-09-29 18:39:36 +0530

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        <div style={{color: "#FFF", textAlign: "left", marginTop: "37px", marginLeft: "40px"}}>MEDIUM</div>
+        <div style={{color: "#FFF", textAlign: "right", marginTop: "-20px", marginRight: "40px"}}>MENU</div>
+        </>
+    )
+};
+
+export default Header;
---
+++ src/components/home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <div>
+            <p style={{color: "#fff", textAlign: "center", fontSize: "13px"}}>..@　%　(　%,,　　　　/　　　　　　#　　　(　　((　.%#　　　　　#　　.(　#　　,..%　　(,/(　@　/　　　%　　(　　　.　　　　　　　((/.(　　%%　.#,　　　　　　　　　#,　%@　　@#/　(　%　　　　#　,　%　/　/,(%　　(,%@</p>
+            <p style={{color: "#fff", textAlign: "center"}}>A LEADING GLOBAL CRYPTO INVESTMENT FUND</p>
+            <p style={{color: "#fff", textAlign: "center", fontSize: "13px"}}>..@　%　(　%,,　　　　/　　　　　%@,(　//(　.　　　#./　,　(　　　　　,　/　　　　#　　#,　@.　.　%　,#..　　%.　　%%　　　　#　　#　@　(　　　　/　　　#　@　.　　%　　　　　　(@　(%　　(.#.　　%.%#//　#(　/　　%/　</p>
+        </div>
+        <div>
+            <h1 style={{color: "#FFF", fontSize: "150px", marginLeft: "30px", fontWeight: "550", marginTop: "20px"}}>DRAGONFLY BACKS</h1>
+            <h1 style={{color: "#FFF", fontSize: "150px", marginLeft: "41%", fontWeight: "550", marginTop: "-140px"}}>YOUR FAVORITE</h1>
+            <h1 style={{color: "#FF4500", fontSize: "130px", marginLeft: "30px", fontWeight: "400", marginTop: "-120px", fontFamily: "sans-serif"}}>DRAGONFLY BACKS</h1>
+            <h1 style={{color: "#FFF", fontSize: "150px", marginLeft: "46%", fontWeight: "550", marginTop: "-50px"}}>GLOBAL FROM</h1>
+            <h1 style={{color: "#FF4500", fontSize: "130px", marginLeft: "46%", fontWeight: "550", marginTop: "-130px"}}>DAY ONE</h1>
+        </div>
+        </>
+    )
+};
+
+export default Home;
